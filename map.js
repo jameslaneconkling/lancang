e2cc85601265a16e215c6dcc88c88a911ca8a6f7
James Conkling <james.lane.conkling@gmail.com>
2014-10-28 09:49:23 -0400

    reindent lines

--- map.js
+++ map.js
@@ -1,66 +1,66 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiaGVsc2lua2kiLCJhIjoia1lFZVlNZyJ9.dVxyXwMZWRmnrXnmOuWAMQ';
 var map = L.mapbox.map('map', 'chinaenvforum.map-cvdwgvbn', {
-    minZoom: 4,
-    maxZoom: 8,
-    maxBounds: [[10,60],    //[[min-y, min-x],
-              [46.5,130]]   //[max-y, max-x]],
-    //zoomControl: false,
-    //attributionControl: false
-})
-    .addLayer(L.mapbox.tileLayer('chinaenvforum.watershed_labels_cn'))
-    .setView([30.0, 98.0], 6);
+  minZoom: 4,
+  maxZoom: 8,
+  maxBounds: [[10,60],    //[[min-y, min-x],
+            [46.5,130]]   //[max-y, max-x]],
+  //zoomControl: false,
+  //attributionControl: false
+  })
+  .addLayer(L.mapbox.tileLayer('chinaenvforum.watershed_labels_cn'))
+  .setView([30.0, 98.0], 6);
 
 
 
 var dams = L.mapbox.featureLayer()
-    .loadURL('data/china_lancang_dams_working_copy_en_cn.geojson')
-    // .setFilter(function(dam){
-    //     return dam.properties['WTRSHED_EN'] === 'Lancang River (Mekong)';
-    // })
-    .on('layeradd', function(d){
-        var dam = d.layer,
-            iconSize = [25,25],
-            shadowSize = [18,12],
-            iconOptions = {
-                iconSize: iconSize,
-                iconAnchor: [iconSize[0]*0.5,iconSize[1]],
-                shadowUrl: 'icon/icon_shadow_30px_a50.png',
-                shadowSize: shadowSize,
-                shadowAnchor: [shadowSize[0]*0.5,shadowSize[1]*0.7]
-            };
+  .loadURL('data/china_lancang_dams_working_copy_en_cn.geojson')
+      // .setFilter(function(dam){
+      //     return dam.properties['WTRSHED_EN'] === 'Lancang River (Mekong)';
+      // })
+  .on('layeradd', function(d){
+    var dam = d.layer,
+    iconSize = [25,25],
+    shadowSize = [18,12],
+    iconOptions = {
+      iconSize: iconSize,
+      iconAnchor: [iconSize[0]*0.5,iconSize[1]],
+      shadowUrl: 'icon/icon_shadow_30px_a50.png',
+      shadowSize: shadowSize,
+      shadowAnchor: [shadowSize[0]*0.5,shadowSize[1]*0.7]
+    };
 
-        if(dam.feature.properties.STATUS_EN === 'Existing Dam'){
-            iconOptions['iconUrl'] = 'icon/wwc_blue.svg';
-            dam.setIcon(L.icon(iconOptions));
-        }else if(dam.feature.properties.STATUS_EN === 'Under Construction'){
-            iconOptions['iconUrl'] = 'icon/wwc_red_stripe.svg';
-            dam.setIcon(L.icon(iconOptions));
-        }else if(dam.feature.properties.STATUS_EN === 'Proposed'){
-            iconOptions['iconUrl'] = 'icon/wwc_l_orange_stripe.svg';
-            dam.setIcon(L.icon(iconOptions));
-        }else if(dam.feature.properties.STATUS_EN === 'Unknown'){
-            iconOptions['iconUrl'] = 'icon/wwc_yellow_hollow.svg';
-            dam.setIcon(L.icon(iconOptions));
-        }else if(dam.feature.properties.STATUS_EN === 'Cancelled'){
-            iconOptions['iconUrl'] = 'icon/wwc_gray_dashed_hollow.svg';
-            dam.setIcon(L.icon(iconOptions));
-        }
-    })
-    .addTo(map);
+    if(dam.feature.properties.STATUS_EN === 'Existing Dam'){
+      iconOptions['iconUrl'] = 'icon/wwc_blue.svg';
+      dam.setIcon(L.icon(iconOptions));
+    }else if(dam.feature.properties.STATUS_EN === 'Under Construction'){
+      iconOptions['iconUrl'] = 'icon/wwc_red_stripe.svg';
+      dam.setIcon(L.icon(iconOptions));
+    }else if(dam.feature.properties.STATUS_EN === 'Proposed'){
+      iconOptions['iconUrl'] = 'icon/wwc_l_orange_stripe.svg';
+      dam.setIcon(L.icon(iconOptions));
+    }else if(dam.feature.properties.STATUS_EN === 'Unknown'){
+      iconOptions['iconUrl'] = 'icon/wwc_yellow_hollow.svg';
+      dam.setIcon(L.icon(iconOptions));
+    }else if(dam.feature.properties.STATUS_EN === 'Cancelled'){
+      iconOptions['iconUrl'] = 'icon/wwc_gray_dashed_hollow.svg';
+      dam.setIcon(L.icon(iconOptions));
+    }
+  })
+  .addTo(map);
 
 
 dams.on('ready', function(){
-    var damCount = -1;
-    document.getElementById('button').addEventListener('click',cycleDams);
+  var damCount = -1;
+  document.getElementById('button').addEventListener('click',cycleDams);
 
-    function cycleDams(){
-        damCount += 1;
+  function cycleDams(){
+    damCount += 1;
 
-        dams.setFilter(function(dam){
-            if(damCount === -1){return true;}
-            return dam.properties['id'] <= damCount;
-        });
-    }
+    dams.setFilter(function(dam){
+      if(damCount === -1){return true;}
+      return dam.properties['id'] <= damCount;
+    });
+  }
 
 });
 
